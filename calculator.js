3de61dc3efb078e09f73074032e0b9b4e7fa0902
Gavin O Brien <142741214+Gaviobm1@users.noreply.github.com>
2023-09-24 23:58:29 +0200

    Add keyboard support

--- calculator.js
+++ calculator.js
@@ -1,7 +1,15 @@
 const container = document.querySelector('.container')
 
+const bigNum = document.createElement('div')
+bigNum.classList.add('bigNum')
+
+const keepTrack = document.createElement('div')
+keepTrack.classList.add('tracking')
+bigNum.appendChild(keepTrack)
+
 const screen = document.createElement('div')
 screen.classList.add('screen')
+bigNum.appendChild(screen)
 
 const buttons = document.createElement('div')
 buttons.classList.add('btns')
@@ -13,15 +21,15 @@ butoClear.classList.add('butoClear', 'top')
 butoClear.textContent = 'C'
 numbers.appendChild(butoClear)
 
+const del = document.createElement('button');
+del.classList.add('del', 'top');
+del.textContent = 'del'
+numbers.appendChild(del)
+
 const percent = document.createElement('button');
 percent.classList.add('percent', 'oper', 'top');
 percent.textContent = ' % ';
 numbers.appendChild(percent);
-
-const negPosi = document.createElement('button');
-negPosi.classList.add('negPosi', 'top');
-negPosi.textContent = '- / +'
-numbers.appendChild(negPosi)
 
 const butoNum1 = document.createElement('button')
 butoNum1.classList.add('numBut', 'num1', 'buto')
@@ -73,7 +81,7 @@ butoNum0.classList.add('numBut', 'num0', 'buto')
 butoNum0.textContent = '0';
 numbers.appendChild(butoNum0)
 
-let butoNumPoint = document.createElement('button')
+const butoNumPoint = document.createElement('button')
 butoNumPoint.classList.add('numBut', 'numPoint')
 butoNumPoint.textContent = '.';
 numbers.appendChild(butoNumPoint)
@@ -103,46 +111,224 @@ divy.classList.add('oper', 'divide')
 divy.textContent = ' ÷ ';
 functions.appendChild(divy);
 
-
 const equals = document.createElement('button')
 equals.classList.add('equal')
 equals.textContent = ' = ';
 functions.appendChild(equals);
 
+const footer = document.createElement('div')
+footer.classList.add('footer')
+footer.textContent = 'Gaviobm © 2023'
 
-container.appendChild(screen);
+
+container.appendChild(bigNum);
 container.appendChild(buttons);
 buttons.appendChild(numbers);
 buttons.appendChild(functions);
+document.body.appendChild(footer)
+document.body.style.backgroundColor = '#FEC5E5'
 
 
 
 let clickCounter = 0;
+let clickCounterx = 0;
 let displayValue = [];
 let num1;
 let num2; 
 let operator;
 let str1 = ''
 let arr1;
-let buttonOpacity = document.querySelectorAll('.oper');
 
-const clear = document.querySelector('.butoClear').addEventListener('click', () => {
+document.addEventListener('keydown', (e) => {
+  
+    if (e.key == 1 || e.key == 2 || e.key == 3 || e.key == 4 || e.key == 5 || e.key == 6 || e.key == 7 || e.key == 8 || e.key == 9 || e.key == 0) {
+    if (clickCounter < 1) {
+            screen.textContent = '';
+    }
+
+    keepTrack.textContent += e.key;
+    screen.textContent += e.key;
+    clickCounter++;
+    }
+   
+    if (e.key == '*'){ 
+    if (displayValue.length >= 2) {
+    keepTrack.textContent += ' x ';
+    displayValue.push(screen.textContent);
+    str1=  displayValue.join('');
+    arr1= str1.split(' ');
+    num1 = arr1[0];
+    num2 = arr1[2];
+    operator = arr1 [1];
+    screen.textContent = operate(num1, num2, operator);
+    displayValue = [];
+    displayValue.push(screen.textContent);
+    displayValue.push(` ${e.key} `);
+    clickCounter = 0; 
+    } else { 
+    keepTrack.textContent += ' x ';
+    clickCounter = 0;
+    displayValue.push(screen.textContent);
+    displayValue.push(` ${e.key} `);  
+    clickCounter = 0;
+    if (displayValue[0] == displayValue[1]) {
+        displayValue.splice(0,1);
+    }     
+    }}
+
+    if (e.key == '+'){ 
+    if (displayValue.length >= 2) {
+    keepTrack.textContent += ' + ';
+    displayValue.push(screen.textContent);
+    str1=  displayValue.join('');
+    arr1= str1.split(' ');
+    num1 = arr1[0];
+    num2 = arr1[2];
+    operator = arr1 [1];
+    screen.textContent = operate(num1, num2, operator);
+    displayValue = [];
+    displayValue.push(screen.textContent);
+    displayValue.push(` ${e.key} `);
+    clickCounter = 0; 
+    } else { 
+    keepTrack.textContent += ' + ';
+    clickCounter = 0;
+    displayValue.push(screen.textContent);
+    displayValue.push(` ${e.key} `);  
+    clickCounter = 0;
+    if (displayValue[0] == displayValue[1]) {
+        displayValue.splice(0,1);
+    }    
+    }}
+
+    if (e.key == '-'){ 
+    if (displayValue.length >= 2) {
+    keepTrack.textContent += ' - ';
+    displayValue.push(screen.textContent);
+    str1=  displayValue.join('');
+    arr1= str1.split(' ');
+    num1 = arr1[0];
+    num2 = arr1[2];
+    operator = arr1 [1];
+    screen.textContent = operate(num1, num2, operator);
+    displayValue = [];
+    displayValue.push(screen.textContent);
+    displayValue.push(` ${e.key} `);
+    clickCounter = 0; 
+    } else { 
+    keepTrack.textContent += ' - ';
+    clickCounter = 0;
+    displayValue.push(screen.textContent);
+    displayValue.push(` ${e.key} `);  
+    clickCounter = 0;  
+    if (displayValue[0] == displayValue[1]) {
+        displayValue.splice(0,1);
+    }   
+    }}
+
+    if (e.key == '/'){ 
+    if (displayValue.length >= 2) {
+    keepTrack.textContent += ' ÷ ';
+    displayValue.push(screen.textContent);
+    str1=  displayValue.join('');
+    arr1= str1.split(' ');
+    num1 = arr1[0];
+    num2 = arr1[2];
+    operator = arr1 [1];
+    screen.textContent = operate(num1, num2, operator);
+    displayValue = [];
+    displayValue.push(screen.textContent);
+    displayValue.push(` ${e.key} `);
+    clickCounter = 0; 
+    } else { 
+    keepTrack.textContent += ' ÷ ';
+    clickCounter = 0;
+    displayValue.push(screen.textContent);
+    displayValue.push(` ${e.key} `);  
+    clickCounter = 0;     
+    if (displayValue[0] == displayValue[1]) {
+        displayValue.splice(0,1);
+    }
+    }}
+
+    if (e.key == '%') {
+    if (displayValue.length >= 2) {
+    keepTrack.textContent += ' % ';
+    displayValue.push(screen.textContent);
+    str1=  displayValue.join('');
+    arr1= str1.split(' ');
+    num1 = arr1[0];
+    num2 = arr1[2];
+    operator = arr1 [1];
+    screen.textContent = operate(num1, num2, operator);
+    displayValue = [];
+    displayValue.push(screen.textContent);
+    displayValue.push(` ${e.key} `);
+    clickCounter = 0; 
+    } else { 
+    keepTrack.textContent += ' % ';
+    clickCounter = 0;
+    displayValue.push(screen.textContent);
+    displayValue.push(` ${e.key} `);  
+    clickCounter = 0; 
+    if (displayValue[0] == displayValue[1]) {
+        displayValue.splice(0,1);
+    }    
+    }}
+   
+   if (e.key == 'c') {
+    keepTrack.textContent = '';
     screen.textContent = '';
     displayValue = [];
+   }
+ 
+   if (e.key == 'Backspace') {
+   screen.textContent = screen.textContent.slice(0, -1);
+   keepTrack.textContent = keepTrack.textContent.slice(0, -1);
+   }
+
+   
+   if (e.key == 'Enter') {
+   displayValue.push(screen.textContent)
+   str1=  displayValue.join('');
+   arr1= str1.split(' ');
+   num1 = arr1[0];
+   num2 = arr1[2];
+   operator = arr1 [1];
+   screen.textContent = operate(num1, num2, operator);
+   displayValue = [];
+   displayValue.push(screen.textContent);
+   }
+   if (e.key == '.') {
+   if (screen.textContent.includes('.')) {
+    butoNumPoint = null;
+   }
+   screen.textContent += butoNumPoint.textContent;
+   keepTrack.textContent += butoNumPoint.textContent
+   }
+
+   
+   
 })
+   
+
 
 const className = document.querySelectorAll('.buto').forEach((button) => button.addEventListener('click', () => {
+    keepTrack.textContent += button.textContent
     if (clickCounter < 1) {
         screen.textContent = '';
     }
+    document.querySelectorAll('.oper').forEach((button) => button.style.border ='none')
     screen.textContent += button.textContent;
     clickCounter++;
 }))
 
 
-
 const screenContent = document.querySelectorAll('.oper').forEach((button) => button.addEventListener('click', () => {
+    
    if (displayValue.length >= 2) {
+        keepTrack.textContent += button.textContent
+        button.style.border = '5px solid black';
         displayValue.push(screen.textContent);
         str1=  displayValue.join('');
         arr1= str1.split(' ');
@@ -154,11 +340,14 @@ const screenContent = document.querySelectorAll('.oper').forEach((button) => but
         displayValue.push(screen.textContent);
         displayValue.push(button.textContent);
         clickCounter = 0; 
+        clickCounterx = 0;
     } else {
+    keepTrack.textContent += button.textContent
     clickCounter = 0; 
+    clickCounterx = 0;
     displayValue.push(screen.textContent);
     displayValue.push(button.textContent);
-    button.style.opacity = '0.5';
+    button.style.border = '5px solid black';
     screen.textContent = displayValue[0];
     if (displayValue[0] == displayValue[1]) {
         displayValue.splice(0,1);
@@ -166,34 +355,41 @@ const screenContent = document.querySelectorAll('.oper').forEach((button) => but
         
 }))
 
+const getResult = document.querySelector('.equal').addEventListener('click', () => {
+    
+    displayValue.push(screen.textContent)
+    str1=  displayValue.join('');
+    arr1= str1.split(' ');
+    num1 = arr1[0];
+    num2 = arr1[2];
+    operator = arr1 [1];
+    screen.textContent = operate(num1, num2, operator);
+    displayValue = [];
+    displayValue.push(screen.textContent);
+ })
+
+const upperCalc = document.querySelector('.tracking').addEventListener('click', () => upperCalc.textContent = displayValue)
+
+const clear = document.querySelector('.butoClear').addEventListener('click', () => {
+    keepTrack.textContent = '';
+    screen.textContent = '';
+    displayValue = [];
+    document.querySelectorAll('.oper').forEach((button) => button.style.border ='none')
+})
+
 const onePoint = document.querySelector('.numPoint').addEventListener('click', () => {
     if (screen.textContent.includes('.')) {
         butoNumPoint = null;
 }
     screen.textContent += butoNumPoint.textContent;
-    
+    keepTrack.textContent += butoNumPoint.textContent
     
 })
 
-const getResult = document.querySelector('.equal').addEventListener('click', () => {
-   displayValue.push(screen.textContent)
-   str1=  displayValue.join('');
-   arr1= str1.split(' ');
-   num1 = arr1[0];
-   num2 = arr1[2];
-   operator = arr1 [1];
-   screen.textContent = operate(num1, num2, operator);
-   displayValue = [];
-   displayValue.push(screen.textContent);
-   
-})
+const negyPosy = document.querySelector('.del').addEventListener('click', () => {
+    screen.textContent = screen.textContent.slice(0, -1);
+    keepTrack.textContent = keepTrack.textContent.slice(0, -1);
 
-const negyPosy = document.querySelector('.negPosi').addEventListener('click', () => {
-    if (!screen.textContent.includes('-')) {
-     screen.textContent = '-' + screen.textContent
-    } else if (screen.textContent.includes('-')) {
-        screen.textContent = (+screen.textContent) + (+screen.textContent * -2)
-    }
 })
 
 function addUp (a, b) {
@@ -239,15 +435,18 @@ function operate (a, b, c) {
     if (c == '-') {
            return subtract(a, b);
     }
-    if (c == 'x') {
+    if (c == 'x' || c == '*') {
            return multiply(a, b);
     }
-    if ( c == '÷') {
+    if ( c == '÷' || c == '/') {
             return divide(a, b);
     }
     if (c == '%') {
         return percentage(a, b); 
     }
 }
+
+
+
 
 
